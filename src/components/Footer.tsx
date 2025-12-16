c4dc25677735fead2db3dc1eae0033c9dc6dc46a
pantoine31 <antonhspap@icloud.com>
2025-12-17 00:34:09 +0200

    pdfs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <div className="footer-links">
                 <a href="https://linkedin.com/in/antoni-dev" target="_blank">LinkedIn</a>
                 <a href="https://github.com/pantoine31" target="_blank">GitHub</a>
-                <a href="/resume.pdf" target="_blank">Resume</a>
+                <a href="/pdfs/papakonstantinou_cv.pdf" target="_blank">Resume</a>
                 <a href="mailto:antonhspap@icloud.com">Contact via E-mail</a>
             </div>
 
