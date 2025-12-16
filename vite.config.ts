f63f5b4453f75c4fee97e228421625f5b664a643
pantoine31 <antonhspap@icloud.com>
2025-12-17 00:13:17 +0200

    fix base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/papantonhs_web/', // 👈 ΤΟ ΟΝΟΜΑ ΤΟΥ REPO ΣΟΥ
+  base: "/"
 })
-
